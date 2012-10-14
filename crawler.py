5e6ec9cd3941d52f370eb260d3c50aae8154be53
Luke Southam <luke@ubuntu.(none)>
2012-10-14 09:28:19 -0700

    Improved documentaion of Page class

--- crawler.py
+++ crawler.py
@@ -34,48 +34,6 @@
 # SUCH DAMAGE.
 """
 A html Page() object with webcrawling capabilitys
-
-Args:
-url (str):          url to web page
-level=1 (int):   level of recursive web crawling.
-                      0;just that page,
-                      1;that page and its links
-                      2;that page, its links and its link's links
-                      etc
-
-Usage:
-
->>> page = Page(url, level=1)
-
->>> vars(page)
-{
-    'url': "...",
-    'source': "...",
-    'title': "...",
-    'description': "...",
-    'rank': "...",
-    'keywords': ["...", ...],
-    'urls': [...],
-    'links': [...]
-}
-
-# Each Page object creates links as Page(url, level-1)
->>> page.links
-[< __name__.Page at ....>,
- < __name__.Page at ....>,
- < __name__.Page at ....>,
- ....]
-
->>> page.links[0]
-< __name__.Page at ....>
-
->>> page = page.links[0]
->>> page.links
-[< __name__.Page at ....>,
- < __name__.Page at ....>,
- < __name__.Page at ....>,
- ....]
-
 """
 
 import urllib2
@@ -90,6 +48,53 @@ __status__ = "Development"
 
 
 class Page(object):
+    """
+    A html Page() object with webcrawling capabilitys
+
+    Args:
+    url (str):          url to web page
+    level=1 (int):   level of recursive web crawling.
+                          0;just that page,
+                          1;that page and its links
+                          2;that page, its links and its link's links
+                          etc
+
+    Usage:
+
+    >>> page = Page(url, level=1)
+
+    >>> vars(page)
+    {
+        'url': "...",                             # url passed through __init__
+        'source': "...",                          # urllib2.urlopen(url).read()
+        'soup': < __name__.BeautifulSoup at ... > # BeautifulSoup(source)
+        'title': "...",                           # title tag
+        'description': "...",                     # meta tag description
+        'rank': "...",                            # google's rank of url
+        'keywords': ["...", ...],                 # meta tag keywords
+        'urls': [...],                            # href of a tags
+        'links': [...]                            # Page instances of urls with
+                                                  # level - 1
+    }
+
+    # Each Page object creates links as Page(url, level-1)
+    >>> page.links
+    [< __name__.Page at ....>,
+     < __name__.Page at ....>,
+     < __name__.Page at ....>,
+     ....]
+
+    >>> page.links[0]
+    < __name__.Page at ....>
+
+    >>> page = page.links[0]
+    >>> page.links
+    [< __name__.Page at ....>,
+     < __name__.Page at ....>,
+     < __name__.Page at ....>,
+     ....]
+
+    """
     def __init__(self, url, level=1):
         self.url = url
         self.level = level
